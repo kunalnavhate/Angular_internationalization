66ddade3685d4d5a63f95c9ef27574a1cf5bb06b
Kunal Navhate <kunal.navhate@gmail.com>
2015-10-06 14:08:19 +0530

    Test commit
    test commit comment

--- app/app.js
+++ app/app.js
@@ -10,13 +10,7 @@
           FOO: 'This is a paragraph.',
           BUTTON_LANG_EN: 'english',
           BUTTON_LANG_DE: 'german'
-        });
-        $translateProvider.translations('de', {
-          TITLE: 'Hallo',
-          FOO: 'Dies ist ein Paragraph.',
-          BUTTON_LANG_EN: 'englisch',
-          BUTTON_LANG_DE: 'deutsch'
-        });
+        });        
         $translateProvider.preferredLanguage('po');
       });
 
